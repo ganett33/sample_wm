68cc772690861de783cb99a73b33411dd0f706fc
sid <ganett33@gmail.com>
2021-08-27 20:46:16 +1200

    temp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import ScrollToTop from './Components/ScrollToTop';
-import Sidebar from '../src/Components/Sidebar/Sidebar';
-import Navbar from '../src/Components/Navbar/Navbar';
 import Home from './pages/home';
 import SignIn from './pages/signin';
 
-
 function App() {
-    const [isOpen, setIsOpen] = useState(false)
-    
-    const toggle = () => {
-      setIsOpen(!isOpen)
-    };
-
     return (
         <Router>
             <ScrollToTop />
-            <Sidebar isOpen={isOpen} toggle={toggle} />
-            <Navbar />
             <Switch>
                 <Route path='/' exact component={Home} />
                 <Route path='/sign-in' component={SignIn} />
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Sidebar from '../Components/Sidebar/Sidebar';
+import Navbar from '../Components/Navbar/Navbar';
 import HeroSection from '../Components/HeroSection/HeroSection';
 import Reviews from '../Components/Reviews/Reviews';
 import Footer from '../Components/Footer/Footer';
@@ -6,9 +8,16 @@ import ImageSlider from '../Components/ImageSlider/ImageSlider';
 import { SliderData } from '../Components/ImageSlider/SliderData';
 
 const Home = () => {
+  const [isOpen, setIsOpen] = useState(false)
     
+  const toggle = () => {
+    setIsOpen(!isOpen)
+  };  
+
     return (
       <>
+        <Sidebar isOpen={isOpen} toggle={toggle} />
+        <Navbar  toggle={toggle}/>
         <HeroSection />
         <Reviews />
         <ImageSlider slides={SliderData} />
